f8c97593b6002e9525312c9037f6de6869105402
prositeee <verlianofc@gmail.com>
2025-06-25 13:54:31 +0700

    Upgrade Portfolio

--- js/script.js
+++ js/script.js
@@ -1,58 +1,71 @@
 /*==================== Toggle Icon Navbar ==================== */
-let menuIcon = document.querySelector('#menu-icon');
-let navbar   = document.querySelector('.navbar');
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
 };
 
 /*==================== Scroll Sections Active Link ==================== */
-let sections = document.querySelectorAll('section');
-let navlinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-    sections.forEach(sec => {
-        let top         =   window.scrollY;
-        let offset      =   sec.offsetTop - 150;
-        let height      =   sec.offsetHeight;
-        let id          =   sec.getAttribute('id');
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-        if(top >= offset && top < offset + height){
-            navlinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-        };
-    });
-    /*==================== Sticky Navbar ==================== */
-    let header = document.querySelector('header');
+    if (top >= offset && top < offset + height) {
+      navlinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+  /*==================== Sticky Navbar ==================== */
+  let header = document.querySelector("header");
 
-    header.classList.toggle('sticky',window.scrollY > 100);
+  header.classList.toggle("sticky", window.scrollY > 100);
 
-    /*==================== Remove Toggle Icon & Navbar When Click Navbar Link(Scroll) ==================== */
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
+  /*==================== Remove Toggle Icon & Navbar When Click Navbar Link(Scroll) ==================== */
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 };
 
 /*==================== Scroll Reveal ==================== */
 ScrollReveal({
-    reset: true, 
-    distance:'80px',
-    duration: 2000,
-    delay:200
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
 });
 
-ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .portfolio-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
 //ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
- /*==================== Typed Js ==================== */
-const typed = new Typed('.multiple-text',{
-    strings: ['WEB DEVELOPER', 'COPY WRITTER', 'WEB DESIGN', 'TRANSLATOR'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
+/*==================== Typed Js ==================== */
+const typed = new Typed(".multiple-text", {
+  strings: [
+    "IT SUPPORT",
+    "DATA ENTRY",
+    "UI/UX",
+    "MICROSOFT OFFICE",
+    "COPY WRITTER",
+    "DATA ANALYST",
+    "TRANSLATOR",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
 });
